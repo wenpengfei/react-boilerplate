a9c5aede55ebd8b244cdf8a744dbc16291ac2b6f
catwen <jquery.min.js@gmail.com>
2016-12-29 06:32:21 +0800

    feat: Update recompose demo

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -26,103 +26,37 @@ import { changeUsername } from './actions';
 import { selectUsername } from './selectors';
 import { selectRepos, selectLoading, selectError } from 'containers/App/selectors';
 
-export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  /**
-   * when initial state username is not null, submit the form to load repos
-   */
-  componentDidMount() {
-    if (this.props.username && this.props.username.trim().length > 0) {
-      this.props.onSubmitForm();
-    }
-  }
+import { bindActionCreators } from 'redux';
+import { compose, withContext, getContext, withHandlers } from 'recompose';
+import * as actions from './actions';
 
-  render() {
-    let mainContent = null;
-
-    // Show a loading indicator when we're loading
-    if (this.props.loading) {
-      mainContent = (<List component={LoadingIndicator} />);
-
-    // Show an error if there is one
-    } else if (this.props.error !== false) {
-      const ErrorComponent = () => (
-        <ListItem item={'Something went wrong, please try again!'} />
-      );
-      mainContent = (<List component={ErrorComponent} />);
-
-    // If we're not loading, don't have an error and there are repos, show the repos
-    } else if (this.props.repos !== false) {
-      mainContent = (<List items={this.props.repos} component={RepoListItem} />);
-    }
-
-    return (
-      <article>
-        <Helmet
-          title="Home Page"
-          meta={[
-            { name: 'description', content: 'A React.js Boilerplate application homepage' },
-          ]}
-        />
-        <div>
-          <CenteredSection>
-            <H2>
-              <FormattedMessage {...messages.startProjectHeader} />
-            </H2>
-            <p>
-              <FormattedMessage {...messages.startProjectMessage} />
-            </p>
-          </CenteredSection>
-          <Section>
-            <H2>
-              <FormattedMessage {...messages.trymeHeader} />
-            </H2>
-            <Form onSubmit={this.props.onSubmitForm}>
-              <label htmlFor="username">
-                <FormattedMessage {...messages.trymeMessage} />
-                <AtPrefix>
-                  <FormattedMessage {...messages.trymeAtPrefix} />
-                </AtPrefix>
-                <Input
-                  id="username"
-                  type="text"
-                  placeholder="mxstbr"
-                  value={this.props.username}
-                  onChange={this.props.onChangeUsername}
-                />
-              </label>
-            </Form>
-            {mainContent}
-          </Section>
-        </div>
-      </article>
-    );
-  }
-}
-
-HomePage.propTypes = {
-  loading: React.PropTypes.bool,
-  error: React.PropTypes.oneOfType([
-    React.PropTypes.object,
-    React.PropTypes.bool,
-  ]),
-  repos: React.PropTypes.oneOfType([
-    React.PropTypes.array,
-    React.PropTypes.bool,
-  ]),
-  onSubmitForm: React.PropTypes.func,
-  username: React.PropTypes.string,
-  onChangeUsername: React.PropTypes.func,
+const HomePage = ({ username, onChangeUsernameCreator }) => {
+  return (
+    <div>
+      <Input
+        id="username"
+        type="text"
+        placeholder="mxstbr"
+        value={username}
+        onChange={onChangeUsernameCreator}
+      />
+    </div>
+  );
 };
 
-export function mapDispatchToProps(dispatch) {
-  return {
-    onChangeUsername: (evt) => dispatch(changeUsername(evt.target.value)),
-    onSubmitForm: (evt) => {
-      if (evt !== undefined && evt.preventDefault) evt.preventDefault();
-      dispatch(loadRepos());
-    },
-  };
-}
+HomePage.propTypes = {
+  onChangeUsernameCreator: React.PropTypes.func,
+};
+
+// export function mapDispatchToProps(dispatch) {
+//   return {
+//     onChangeUsername: (evt) => dispatch(changeUsername(evt.target.value)),
+//     onSubmitForm: (evt) => {
+//       if (evt !== undefined && evt.preventDefault) evt.preventDefault();
+//       dispatch(loadRepos());
+//     },
+//   };
+// }
 
 const mapStateToProps = createStructuredSelector({
   repos: selectRepos(),
@@ -132,4 +66,22 @@ const mapStateToProps = createStructuredSelector({
 });
 
 // Wrap the component to inject dispatch and state into it
-export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+// export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+export default compose(
+  connect(mapStateToProps, (dispatch) => ({
+    actions: bindActionCreators(actions, dispatch),
+  })),
+  withContext({
+    actions: React.PropTypes.object.isRequired,
+  }, (props) => ({
+    actions: props.actions,
+  })),
+  getContext({
+    actions: React.PropTypes.object.isRequired,
+  }),
+  withHandlers({
+    onChangeUsernameCreator: (props) => (event) => {
+      props.actions.changeUsername(event.target.value);
+    },
+  }),
+)(HomePage);
