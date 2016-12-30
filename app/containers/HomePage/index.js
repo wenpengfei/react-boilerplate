8149f88712532124d7242e6e258ec58a7440068d
catwen <jquery.min.js@gmail.com>
2016-12-31 04:35:56 +0800

    style: Update template

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -4,17 +4,17 @@
  * This is the first thing users see of our App, at the '/' route
  */
 
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
-import Input from './Input';
-import { selectUsername } from './selectors';
-import { selectRepos, selectLoading, selectError } from 'containers/App/selectors';
+import Input from './Input'
+import { selectUsername } from './selectors'
+import { selectRepos, selectLoading, selectError } from 'containers/App/selectors'
 
-import { bindActionCreators } from 'redux';
-import { compose, withContext, getContext, withHandlers } from 'recompose';
-import * as actions from './actions';
+import { bindActionCreators } from 'redux'
+import { compose, withContext, getContext, withHandlers } from 'recompose'
+import * as actions from './actions'
 
 const HomePage = ({ username, onChangeUsernameCreator }) => {
   return (
@@ -27,36 +27,36 @@ const HomePage = ({ username, onChangeUsernameCreator }) => {
         onChange={onChangeUsernameCreator}
       />
     </div>
-  );
-};
+  )
+}
 
 HomePage.propTypes = {
   onChangeUsernameCreator: React.PropTypes.func,
   username: React.PropTypes.string,
-};
+}
 
 const mapStateToProps = createStructuredSelector({
   repos: selectRepos(),
   username: selectUsername(),
   loading: selectLoading(),
   error: selectError(),
-});
+})
 
 export default compose(
-  connect(mapStateToProps, (dispatch) => ({
+  connect(mapStateToProps, dispatch => ({
     actions: bindActionCreators(actions, dispatch),
   })),
   withContext({
     actions: React.PropTypes.object.isRequired,
-  }, (props) => ({
+  }, props => ({
     actions: props.actions,
   })),
   getContext({
     actions: React.PropTypes.object.isRequired,
   }),
   withHandlers({
-    onChangeUsernameCreator: (props) => (event) => {
-      props.actions.changeUsername(event.target.value);
+    onChangeUsernameCreator: props => event => {
+      props.actions.changeUsername(event.target.value)
     },
   }),
-)(HomePage);
+)(HomePage)
